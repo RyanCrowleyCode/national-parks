0931d6aaca0a26d7ffa415059837e17f8ff6e987
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-25 13:36:27 -0500

    added boilerplate code to html, linked css and js to index.html

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+// Build an application that gets data about national parks from your json-server API and displays the list of national parks on the DOM. Each national park should be formatted in HTML like below:
+
+// <article>
+//     <h3>Park Name</h3>
+//     <p>State the park in located in</p>
+// </article>
+// If the park has been visited, the article tag should have a red dashed border. If the park has not been visited, it should have a green solid border.

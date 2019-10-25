e04d4b37391a526cc34fcb783a1bfeec51f6b65a
Ryan Crowley <ryancrowleycode@gmail.com>
2019-10-25 15:26:13 -0500

    restored after messing up previous branch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
 // call fetch call function
 // TODO: .then the parks response and send as argument to dom printer function.
-API.getParks()
+parkAPI.getParks()
 .then(parkList => printParksToDOM(parkList)) // put your function call here
--- src/scripts/parkData.js
+++ src/scripts/parkData.js
@@ -1,5 +1,5 @@
 // created object API that fetches from json server and returns a js object
-const API = {
+const parkAPI = {
     getParks: function () {
         return fetch("http://localhost:8088/parks")
         .then(response => response.json())
